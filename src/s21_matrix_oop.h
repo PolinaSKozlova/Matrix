580c8dd2f1dfe95314561da9b41ca76c95a0f34d
verenach <verenach@student.21-school.ru>
2023-01-20 19:34:54 +0700

    переименовала файлы и добавила описание методов в хедер

---
+++ src/s21_matrix_oop.h
@@ -0,0 +1,44 @@
+#ifndef SRC_S21_MATRIX_OOP_H_
+#define SRC_S21_MATRIX_OOP_H_
+
+class S21Matrix {
+ private:
+  // Attributes
+  int rows_, cols_;  // Rows and columns
+  double** matrix_;  // Pointer to the memory where the matrix is allocated
+
+ public:
+  /* Базовый конструктор, инициализирующий матрицу некоторой
+   * заранее заданной размерностью */
+  S21Matrix();
+  /* Параметризированный конструктор с количеством строк и столбцов */
+  S21Matrix(int rows, int cols);
+  /* Конструктор копирования */
+  S21Matrix(const S21Matrix& other);
+  /* Конструктор переноса */
+  S21Matrix(S21Matrix&& other);
+  /* Деструктор */
+  ~S21Matrix();
+
+  /* Проверяет матрицы на равенство между собой */
+  bool EqMatrix(const S21Matrix& other);
+  /* Прибавляет вторую матрицы к текущей */
+  void SumMatrix(const S21Matrix& other);
+  /* Вычитает из текущей матрицы другую */
+  void SubMatrix(const S21Matrix& other);
+  /* Умножает текущую матрицу на число */
+  void MulNumber(const double num);
+  /* Умножает текущую матрицу на вторую */
+  void MulMatrix(const S21Matrix& other);
+  /* Создает новую транспонированную матрицу из текущей и возвращает ее */
+  S21Matrix Transpose();
+  /* Вычисляет матрицу алгебраических дополнений текущей матрицы
+  и возвращает ее */
+  S21Matrix CalcComplements();
+  /* Вычисляет и возвращает определитель текущей матрицы */
+  double Determinant();
+  /* Вычисляет и возвращает обратную матрицу */
+  S21Matrix InverseMatrix();
+}
+
+#endif  // SRC_S21_MATRIX_OOP_H_
