8861f7c86e5c785495dd98a6c5a37b83549c995a
verenach <verenach@student.21-school.ru>
2023-01-22 18:13:49 +0700

    проверила конструкторы и выделение памяти

--- src/main.cc
+++ src/main.cc
@@ -1,22 +1,22 @@
-#include <iostream>
-
 #include "./s21_matrix_oop.h"
 
 using namespace std;
 
 int main() {
-  S21Matrix m;
-  //   S21Matrix m(4, 5);
-  //   int r = m.GetRows();
-  int r;
-  m.GetRows(&r);
+  S21Matrix m(10, 15);
+  int r = m.GetRows();
   int c = m.GetCols();
+  //   cout << "r = " << r << "\n"
+  //        << "c = " << c << endl;
+  //   m.SetRows(8);
+  //   m.SetCols(5);
+  //   r = m.GetRows();
+  //   c = m.GetCols();
+  //   m.MemoryAllocate();
   cout << "r = " << r << "\n"
        << "c = " << c << endl;
-  m.SetRows(8);
-  m.SetCols(5);
-  c = m.GetCols();
-  cout << "r = " << r << "\n"
-       << "c = " << c << endl;
+
+  m.ShowMatrix();
+
   return 0;
 }
