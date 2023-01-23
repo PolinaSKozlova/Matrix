212f2648e8c45a415dcfbcb18eeab12dbd1039d8
verenach <verenach@student.21-school.ru>
2023-01-23 20:30:38 +0700

    исправила сложение матриц

--- src/s21_matrix_oop_methods.cc
+++ src/s21_matrix_oop_methods.cc
@@ -22,7 +22,7 @@ void S21Matrix::SumMatrix(const S21Matrix& other) {
     if (this->rows_ == other.rows_ && this->cols_ == other.cols_) {
       for (int i = 0; i < this->rows_; i++) {
         for (int j = 0; j < this->cols_; j++) {
-          this->matrix_[i][j] = this->matrix_[i][j] + other.matrix_[i][j];
+          this->matrix_[i][j] += other.matrix_[i][j];
         }
       }
     } else {
