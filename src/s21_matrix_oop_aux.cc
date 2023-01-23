9407cdf9ff17a1ec53dcb8476cf4dc1cf2b32671
verenach <verenach@student.21-school.ru>
2023-01-23 20:19:52 +0700

    перенесла доп функции

--- src/s21_matrix_oop_aux.cc
+++ src/s21_matrix_oop_aux.cc
@@ -18,3 +18,32 @@ int S21Matrix::CheckMatrix(const S21Matrix& other) {
              ? ERROR
              : OK;
 }
+
+/************************************************/
+
+void S21Matrix::ShowMatrix() {
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      std::cout << this->matrix_[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << "call function ShowMatrix " << this->matrix_ << std::endl;
+}
+
+void S21Matrix::FillMatrix() {
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      this->matrix_[i][j] = 1 + i * j;
+    }
+  }
+}
+
+void S21Matrix::FillMatrix1() {
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      this->matrix_[i][j] = i + j;
+    }
+  }
+}
+/************************************************/
--- src/s21_matrix_oop_constructors.cc
+++ src/s21_matrix_oop_constructors.cc
@@ -1,34 +1,5 @@
 #include "./s21_matrix_oop.h"
 
-/************************************************/
-
-void S21Matrix::ShowMatrix() {
-  for (int i = 0; i < rows_; i++) {
-    for (int j = 0; j < cols_; j++) {
-      std::cout << this->matrix_[i][j] << " ";
-    }
-    std::cout << std::endl;
-  }
-  std::cout << "call function ShowMatrix " << this->matrix_ << std::endl;
-}
-
-void S21Matrix::FillMatrix() {
-  for (int i = 0; i < rows_; i++) {
-    for (int j = 0; j < cols_; j++) {
-      this->matrix_[i][j] = 1 + i * j;
-    }
-  }
-}
-
-void S21Matrix::FillMatrix1() {
-  for (int i = 0; i < rows_; i++) {
-    for (int j = 0; j < cols_; j++) {
-      this->matrix_[i][j] = i + j;
-    }
-  }
-}
-/************************************************/
-
 S21Matrix::S21Matrix() {
   rows_ = 0;
   cols_ = 0;
