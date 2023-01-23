77ce84c6d431350e471f08a2175e9692607aba34
verenach <verenach@student.21-school.ru>
2023-01-23 20:56:58 +0700

    добавила метод Transpose

--- src/s21_matrix_oop_methods.cc
+++ src/s21_matrix_oop_methods.cc
@@ -29,3 +29,26 @@ void S21Matrix::SumMatrix(const S21Matrix& other) {
     }
   }
 }
+
+// void S21Matrix::SubMatrix(const S21Matrix& other) {}
+
+// void S21Matrix::MulNumber(const double num) {}
+
+// void S21Matrix::MulMatrix(const S21Matrix& other) {}
+
+S21Matrix S21Matrix::Transpose() {
+  if (!(this->CheckMatrix())) {
+    S21Matrix tmp(this->cols_, this->rows_);
+    for (int i = 0; i < this->cols_; i++) {
+      for (int j = 0; j < this->rows_; j++) {
+        tmp.matrix_[i][j] = this->matrix_[j][i];
+      }
+    }
+    *this = tmp;
+  }
+  return *this;
+}
+
+// S21Matrix S21Matrix::CalcComplements() {}
+
+// S21Matrix S21Matrix::InverseMatrix() {}
