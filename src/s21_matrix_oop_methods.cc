ea04bd080cee0bd723284d5a14eaf1b1112704e6
verenach <verenach@student.21-school.ru>
2023-01-24 16:22:14 +0700

    добавила реализацию метода MultNum

--- src/s21_matrix_oop_methods.cc
+++ src/s21_matrix_oop_methods.cc
@@ -8,7 +8,7 @@ void S21Matrix::SumMatrix(const S21Matrix& other) { *this += other; }
 
 void S21Matrix::SubMatrix(const S21Matrix& other) { *this -= other; }
 
-// void S21Matrix::MulNumber(const double num) {}
+void S21Matrix::MulNumber(const double num) { *this *= num; }
 
 void S21Matrix::MulMatrix(const S21Matrix& other) { *this *= other; }
 
