4ec97fbec349145e782e5df2356b936274ab4c52
verenach <verenach@student.21-school.ru>
2023-01-25 13:15:11 +0700

    добавила матрицу миноров

--- src/s21_matrix_oop_aux.cc
+++ src/s21_matrix_oop_aux.cc
@@ -7,8 +7,28 @@ void S21Matrix::MemoryAllocate() {
   }
 }
 
-inline int S21Matrix::IsSquare() {
-  return (this->rows_ == this->cols_) ? OK : ERROR;
+int S21Matrix::IsSquare() { return (this->rows_ == this->cols_) ? OK : ERROR; }
+
+int S21Matrix::SignForDeterminant(const int& num) {
+  return (num % 2 == 0) ? 1 : -1;
+}
+
+S21Matrix& S21Matrix::MinorMatrix(const int& rows, const int& cols,
+                                  const S21Matrix& other) {
+  for (int i_other = 0, i_minor = 0;
+       i_other < other.rows_ && i_minor < this->rows_; i_other++, i_minor++) {
+    if (i_other == rows && i_other < this->rows_) {
+      i_other++;
+    }
+    for (int j_other = 0, j_minor = 0;
+         j_other < other.cols_ && j_minor < this->cols_; j_other++, j_minor++) {
+      if (j_other == cols && j_other < this->cols_) {
+        j_other++;
+      }
+      this->matrix_[i_minor][j_minor] = other.matrix_[i_other][j_other];
+    }
+  }
+  return *this;
 }
 
 /************************************************/
