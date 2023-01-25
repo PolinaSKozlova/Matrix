63efa43dd184231995607d3700e1aaf2e34e3542
verenach <verenach@student.21-school.ru>
2023-01-25 12:47:47 +0700

    перенесла доп функции

--- src/s21_matrix_oop_aux.cc
+++ src/s21_matrix_oop_aux.cc
@@ -1,5 +1,16 @@
 #include "./s21_matrix_oop.h"
 
+void S21Matrix::MemoryAllocate() {
+  this->matrix_ = new double*[rows_];
+  for (int i = 0; i < rows_; i++) {
+    this->matrix_[i] = new double[cols_]{};
+  }
+}
+
+inline int S21Matrix::IsSquare() {
+  return (this->rows_ == this->cols_) ? OK : ERROR;
+}
+
 /************************************************/
 
 void S21Matrix::ShowMatrix() {
