3690edd4e44c9ad05b8935992d2b0a1e2e99950c
verenach <verenach@student.21-school.ru>
2023-01-26 22:01:05 +0700

    удалила лишний this

--- src/s21_matrix_oop_constructors.cc
+++ src/s21_matrix_oop_constructors.cc
@@ -18,7 +18,7 @@ S21Matrix::S21Matrix(const S21Matrix& other)
   MemoryAllocate();
   for (int i = 0; i < rows_; i++) {
     for (int j = 0; j < cols_; j++) {
-      this->matrix_[i][j] = other.matrix_[i][j];
+      matrix_[i][j] = other.matrix_[i][j];
     }
   }
 }
@@ -30,13 +30,12 @@ S21Matrix::S21Matrix(S21Matrix&& other) : rows_(0), cols_(0), matrix_(nullptr) {
 }
 
 S21Matrix::~S21Matrix() {
-  std::cout << "Destruct " << matrix_ << std::endl;
   if (matrix_) {
     MemoryFree();
-    matrix_ = nullptr;
-    rows_ = 0;
-    cols_ = 0;
   }
+  matrix_ = nullptr;
+  rows_ = 0;
+  cols_ = 0;
 }
 
 int S21Matrix::GetRows() { return rows_; };
@@ -45,14 +44,14 @@ int S21Matrix::GetCols() { return cols_; }
 
 void S21Matrix::SetRows(const int& r) {
   if (r > 0) {
-    S21Matrix tmp(r, this->cols_);
-    for (int i = 0; i < std::min(this->rows_, r); i++) {
+    S21Matrix tmp(r, cols_);
+    for (int i = 0; i < std::min(rows_, r); i++) {
       for (int j = 0; j < tmp.cols_; j++) {
-        tmp.matrix_[i][j] = this->matrix_[i][j];
+        tmp.matrix_[i][j] = matrix_[i][j];
       }
     }
     *this = tmp;
-    this->rows_ = r;
+    rows_ = r;
   } else {
     throw std::runtime_error(
         "The rows of matrix are less than or equal to zero");
@@ -61,16 +60,16 @@ void S21Matrix::SetRows(const int& r) {
 
 void S21Matrix::SetCols(const int& c) {
   if (c > 0) {
-    S21Matrix tmp(this->rows_, c);
-    for (int i = 0; i < this->rows_; i++) {
-      for (int j = 0; j < std::min(this->cols_, c); j++) {
-        if (j < this->cols_) {
-          tmp.matrix_[i][j] = this->matrix_[i][j];
+    S21Matrix tmp(rows_, c);
+    for (int i = 0; i < rows_; i++) {
+      for (int j = 0; j < std::min(cols_, c); j++) {
+        if (j < cols_) {
+          tmp.matrix_[i][j] = matrix_[i][j];
         }
       }
     }
     *this = tmp;
-    this->cols_ = c;
+    cols_ = c;
   } else {
     throw std::runtime_error(
         "The cols of matrix are less than or equal to zero");
