149b1b0bcc5c923bb5c793bc7ff1363f6638126f
verenach <verenach@student.21-school.ru>
2023-01-26 22:24:25 +0700

    исправила ошиьки в функциях

--- src/s21_matrix_oop_operators.cc
+++ src/s21_matrix_oop_operators.cc
@@ -3,29 +3,57 @@
 static double FABS(double num) { return (num < 0 ? -num : num); }
 
 S21Matrix S21Matrix::operator+(const S21Matrix& other) {
-  S21Matrix tmp(rows_, cols_);
-  *this += other;
-  tmp = *this;
-  return tmp;
+  if (rows_ == other.rows_ && cols_ == other.cols_) {
+    S21Matrix tmp(rows_, cols_);
+    for (int i = 0; i < tmp.rows_; i++) {
+      for (int j = 0; j < tmp.cols_; j++) {
+        tmp.matrix_[i][j] = this->matrix_[i][j] + other.matrix_[i][j];
+      }
+    }
+    return tmp;
+  } else {
+    throw std::runtime_error("Different size of matrices");
+  }
 }
 
 S21Matrix S21Matrix::operator-(const S21Matrix& other) {
-  S21Matrix tmp(this->rows_, this->cols_);
-  *this -= other;
-  tmp = *this;
-  return tmp;
+  if (this->rows_ == other.rows_ && this->cols_ == other.cols_) {
+    S21Matrix tmp(this->rows_, this->cols_);
+    for (int i = 0; i < this->rows_; i++) {
+      for (int j = 0; j < this->cols_; j++) {
+        tmp.matrix_[i][j] = this->matrix_[i][j] - other.matrix_[i][j];
+      }
+    }
+    return tmp;
+  } else {
+    throw std::runtime_error("Different size of matrices");
+  }
 }
 
 S21Matrix S21Matrix::operator*(const S21Matrix& other) {
-  S21Matrix tmp(this->rows_, other.cols_);
-  *this *= other;
-  tmp = *this;
-  return tmp;
+  if (this->cols_ == other.rows_) {
+    S21Matrix tmp(this->rows_, other.cols_);
+    for (int i = 0; i < tmp.rows_; i++) {
+      for (int j = 0; j < tmp.cols_; j++) {
+        for (int k = 0; k < this->cols_; k++) {
+          tmp.matrix_[i][j] += this->matrix_[i][k] * other.matrix_[k][j];
+        }
+      }
+    }
+    return tmp;
+  } else {
+    throw std::runtime_error(
+        "The rows of first matrix are not equal to the cols of second matrix");
+  }
 }
 
 S21Matrix S21Matrix::operator*(const double& num) noexcept {
   S21Matrix tmp(this->rows_, this->cols_);
-  tmp *= num;
+  for (int i = 0; i < this->rows_; i++) {
+    for (int j = 0; j < this->cols_; j++) {
+      tmp.matrix_[i][j] = this->matrix_[i][j] * num;
+    }
+  }
   return tmp;
 }
 
@@ -67,56 +95,19 @@ S21Matrix& S21Matrix::operator=(const S21Matrix& other) noexcept {
 }
 
 S21Matrix& S21Matrix::operator+=(const S21Matrix& other) {
-  if (rows_ == other.rows_ && cols_ == other.cols_) {
-    for (int i = 0; i < rows_; i++) {
-      for (int j = 0; j < cols_; j++) {
-        matrix_[i][j] += other.matrix_[i][j];
-      }
-    }
-    return *this;
-  } else {
-    throw std::runtime_error("Different size of matrices");
-  }
+  return (*this = *this + other);
 }
 
 S21Matrix& S21Matrix::operator-=(const S21Matrix& other) {
-  if (rows_ == other.rows_ && cols_ == other.cols_) {
-    for (int i = 0; i < rows_; i++) {
-      for (int j = 0; j < cols_; j++) {
-        matrix_[i][j] -= other.matrix_[i][j];
-      }
-    }
-    return *this;
-  } else {
-    throw std::runtime_error("Different size of matrices");
-  }
+  return (*this = *this - other);
 }
 
 S21Matrix& S21Matrix::operator*=(const S21Matrix& other) {
-  if (cols_ == other.rows_) {
-    S21Matrix tmp(rows_, other.cols_);
-    for (int i = 0; i < tmp.rows_; i++) {
-      for (int j = 0; j < tmp.cols_; j++) {
-        for (int k = 0; k < cols_; k++) {
-          tmp.matrix_[i][j] += matrix_[i][k] * other.matrix_[k][j];
-        }
-      }
-    }
-    *this = tmp;
-    return *this;
-  } else {
-    throw std::runtime_error(
-        "The rows of first matrix are not equal to the cols of second matrix");
-  }
+  return (*this = *this * other);
 }
 
 S21Matrix& S21Matrix::operator*=(const double& num) noexcept {
-  for (int i = 0; i < rows_; i++) {
-    for (int j = 0; j < cols_; j++) {
-      matrix_[i][j] *= num;
-    }
-  }
-  return *this;
+  return (*this = *this * num);
 }
 
 double& S21Matrix::operator()(const int& i, const int& j) {
