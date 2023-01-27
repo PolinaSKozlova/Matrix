c08e918f6274b75b7f01a15edc84767a228bab76
verenach <verenach@student.21-school.ru>
2023-01-27 13:45:41 +0700

    перенесла инициализацию переменных

--- src/s21_matrix_oop_constructors.cc
+++ src/s21_matrix_oop_constructors.cc
@@ -2,10 +2,9 @@
 
 #include "./s21_matrix_oop.h"
 
-S21Matrix::S21Matrix() {
-  rows_ = 0;
-  cols_ = 0;
-  matrix_ = nullptr;
+S21Matrix::S21Matrix() : rows_(0), cols_(0) {
+  // matrix_ = nullptr;
+  MemoryAllocate();
 }
 
 S21Matrix::S21Matrix(int r, int c) : rows_(r), cols_(c) {
@@ -43,35 +42,37 @@ int S21Matrix::GetRows() { return rows_; };
 int S21Matrix::GetCols() { return cols_; }
 
 void S21Matrix::SetRows(const int& r) {
-  if (r > 0) {
-    S21Matrix tmp(r, cols_);
-    for (int i = 0; i < std::min(rows_, r); i++) {
-      for (int j = 0; j < tmp.cols_; j++) {
-        tmp.matrix_[i][j] = matrix_[i][j];
-      }
+  // if (r > 0) {
+  assert(r > 0);
+  S21Matrix tmp(r, cols_);
+  for (int i = 0; i < std::min(rows_, r); i++) {
+    for (int j = 0; j < tmp.cols_; j++) {
+      tmp.matrix_[i][j] = matrix_[i][j];
     }
-    *this = tmp;
-    rows_ = r;
-  } else {
-    throw std::runtime_error(
-        "The rows of matrix are less than or equal to zero");
   }
+  *this = tmp;
+  rows_ = r;
+  // } else {
+  //   throw std::runtime_error(
+  //       "The rows of matrix are less than or equal to zero");
+  // }
 }
 
 void S21Matrix::SetCols(const int& c) {
-  if (c > 0) {
-    S21Matrix tmp(rows_, c);
-    for (int i = 0; i < rows_; i++) {
-      for (int j = 0; j < std::min(cols_, c); j++) {
-        if (j < cols_) {
-          tmp.matrix_[i][j] = matrix_[i][j];
-        }
+  // if (c > 0) {
+  assert(c > 0);
+  S21Matrix tmp(rows_, c);
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < std::min(cols_, c); j++) {
+      if (j < cols_) {
+        tmp.matrix_[i][j] = matrix_[i][j];
       }
     }
-    *this = tmp;
-    cols_ = c;
-  } else {
-    throw std::runtime_error(
-        "The cols of matrix are less than or equal to zero");
   }
+  *this = tmp;
+  cols_ = c;
+  // } else {
+  //   throw std::runtime_error(
+  //       "The cols of matrix are less than or equal to zero");
+  // }
 }
