17f01c1da3aedec284cd31b417d999227b58b7ac
verenach <verenach@student.21-school.ru>
2023-01-27 19:20:36 +0700

    перенсла константу внутрь класса

--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -6,47 +6,44 @@
 
 enum STATUS { OK, ERROR };
 
-const double& NumAccuracy = 1e-7L;
-
 class S21Matrix {
  public:
-  /* Базовый конструктор, инициализирующий матрицу некоторой
-   * заранее заданной размерностью */
+  const double NumAccuracy = 1e-7L;
+
   S21Matrix();
-  /* Параметризированный конструктор с количеством строк и столбцов */
   S21Matrix(int r, int c);
-  /* Конструктор копирования */
   S21Matrix(const S21Matrix& other);
-  /* Конструктор переноса */
   S21Matrix(S21Matrix&& other);
-  /* Деструктор */
+
+  S21Matrix operator+(const S21Matrix& other);
+  S21Matrix operator-(const S21Matrix& other);
+  S21Matrix operator*(const S21Matrix& other);
+  S21Matrix operator*(const double& num) noexcept;
+  bool operator==(const S21Matrix& other) noexcept;
+  S21Matrix& operator=(const S21Matrix& other) noexcept;
+  S21Matrix& operator+=(const S21Matrix& other);
+  S21Matrix& operator-=(const S21Matrix& other);
+  S21Matrix& operator*=(const S21Matrix& other);
+  S21Matrix& operator*=(const double& num) noexcept;
+  double& operator()(const int& i, const int& j);
+
   ~S21Matrix();
 
-  /* геттеры для получения столбцов и колонок */
   int GetRows();
   int GetCols();
   void SetRows(const int& r);
   void SetCols(const int& c);
 
-  /* Проверяет матрицы на равенство между собой */
   bool EqMatrix(const S21Matrix& other) noexcept;
-  /* Прибавляет вторую матрицы к текущей */
   void SumMatrix(const S21Matrix& other);
-  /* Вычитает из текущей матрицы другую */
   void SubMatrix(const S21Matrix& other);
-  /* Умножает текущую матрицу на число */
   void MulNumber(const double& num) noexcept;
-  /* Умножает текущую матрицу на вторую */
   void MulMatrix(const S21Matrix& other);
-  /* Создает новую транспонированную матрицу из текущей и возвращает ее */
   S21Matrix Transpose() noexcept;
-  /* Вычисляет матрицу алгебраических дополнений текущей матрицы
-  и возвращает ее */
   S21Matrix CalcComplements();
-  /* Вычисляет и возвращает определитель текущей матрицы */
   double Determinant();
-  /* Вычисляет и возвращает обратную матрицу */
   S21Matrix InverseMatrix();
+
   void MemoryAllocate();
   void MemoryFree();
   int IsSquare();
@@ -60,26 +57,11 @@ class S21Matrix {
   void FillMatrix();
   void FillSimpleMatrix();
   /************************************************/
-
-  /*noexcept  Этот спецификатор указывает, что наша функция не генерирует (не
-    выбрасывает) исключение или аварийно завершает свою работу. */
-  S21Matrix operator+(const S21Matrix& other);
-  S21Matrix operator-(const S21Matrix& other);
-  S21Matrix operator*(const S21Matrix& other);
-  S21Matrix operator*(const double& num) noexcept;
-  bool operator==(const S21Matrix& other) noexcept;
-  S21Matrix& operator=(const S21Matrix& other) noexcept;
-  S21Matrix& operator+=(const S21Matrix& other);
-  S21Matrix& operator-=(const S21Matrix& other);
-  S21Matrix& operator*=(const S21Matrix& other);
-  S21Matrix& operator*=(const double& num) noexcept;
-  double& operator()(const int& i, const int& j);
-
   friend double** PtrToMatrix(const S21Matrix& other);
 
  private:
-  int rows_, cols_;  // Rows and columns
-  double** matrix_;  // Pointer to the memory where the matrix is allocated
+  int rows_, cols_;
+  double** matrix_;
 };
 
 S21Matrix operator*(const double& num, const S21Matrix& other) noexcept;
